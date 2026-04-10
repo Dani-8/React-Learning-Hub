1a1593cc66660d47acd21bb3724a79111a64f738
dani-8 <daniyalw6190@gmail.com>
2026-04-11 01:26:11 +0500

    update

---
+++ Concepts Directory/ReduxToolkit/app.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.hook-section')
+    const navLinks = document.querySelectorAll('.nav-link')
+    let current = "";
+    sections.forEach(section => {
+        if (window.pageYOffset >= section.offsetTop - 270) {
+            current = section.getAttribute('id')
+        }
+    })
+    navLinks.forEach(link => {
+        link.classList.remove('active')
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active')
+        }
+    })
+})
